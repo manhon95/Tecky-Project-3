e14ce31e2f03391b5fcab12d48def03ed0661748
VIctor Law <manhon95@gmail.com>
2023-05-22 17:49:25 +0800

    [backend] update auth dto

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignInDto } from './dto/sign-in.dto';
+import { CreateUserDto } from '../models/users/dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -7,7 +9,15 @@ export class AuthController {
 
   @HttpCode(HttpStatus.OK)
   @Post('sign-in')
-  signIn(@Body() signInDto: Record<string, any>) {
+  signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto.email, signInDto.password);
+  }
+  @Post('sign-up')
+  signUp(@Body() signUpDto: CreateUserDto) {
+    return this.authService.signUp(
+      signUpDto.email,
+      signUpDto.username,
+      signUpDto.password,
+    );
   }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -14,14 +14,30 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async signIn(email: string, pass: string): Promise<any> {
+  async signIn(email: string, password: string): Promise<any> {
     const user = await this.usersService.findOneByEmail(email);
     if (!user) {
       throw new NotFoundException();
     }
-    if (!(await bcrypt.compare(pass, user.password))) {
+    if (!(await bcrypt.compare(password, user.password))) {
       throw new UnauthorizedException();
     }
+    const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+
+  async signUp(
+    email: string,
+    username: string,
+    password: string,
+  ): Promise<any> {
+    const user = await this.usersService.create({
+      email,
+      username,
+      password,
+    });
     const payload = { email: user.email, sub: user.id };
     return {
       access_token: await this.jwtService.signAsync(payload),
