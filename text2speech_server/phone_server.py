df4b3d0d3b513f1f8fc661e3a6b6f3d123924d0f
VIctor Law <manhon95@gmail.com>
2023-06-06 17:18:01 +0800

    [text2speech] basic phone server completed

--- text2speech_server/phone_server.py
+++ text2speech_server/phone_server.py
@@ -1,5 +1,5 @@
 from speech2test2speech import answer_question
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 import uvicorn
 
 from twilio.twiml.voice_response import VoiceResponse
@@ -7,31 +7,40 @@ from twilio.twiml.voice_response import VoiceResponse
 app = FastAPI()
 
 
+# @app.get("/")
+# async def greeting():
+#     return "hello"
+
 @app.post("/")
 async def answer_call():
-    resp = VoiceResponse()
+    try:
+        resp = VoiceResponse()
 
-    resp.say("Hello. Please leave a message after the beep.")
-    resp.record(action="/replay")
-    resp.hangup()
+        resp.say("Hello. Please leave a message after the beep.")
+        resp.record(action="/replay")
+        resp.hangup()
 
-    return str(resp)
+        return Response(content=str(resp), media_type="text/xml")
+    except Exception as e:
+        print(e)
 
 
 @app.post("/replay")
 async def answer_call(request: Request):
-    input_recording_url = request.body.RecordingUrl
+    req = await request.form()
+    input_recording_url = req['RecordingUrl']
+    print(input_recording_url)
 
-    text = answer_question(input_recording_url, 2)
+    # text = answer_question(input_recording_url, 2)
     resp = VoiceResponse()
-    if not output_recording_url:
+    if input_recording_url == None:
         resp.say("Error please call again")
     else:
-        resp.play(output_recording_url)
+        resp.play(input_recording_url)
 
     resp.hangup()
 
-    return str(resp)
+    return Response(content=str(resp), media_type="text/xml")
 
 
 # -------------------------------- init server ------------------------------- #
