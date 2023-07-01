2167c2ac4b75c0364ee941010d8f286f49f8a416
VIctor Law <manhon95@gmail.com>
2023-07-01 15:30:44 +0800

    [phone server] update

--- text2speech_server/phone_server.py
+++ text2speech_server/phone_server.py
@@ -1,15 +1,24 @@
-from speech2test2speech import answer_question
+from speech2test2speech import answer_question_from_url
 from fastapi import FastAPI, Request, Response
+from fastapi.staticfiles import StaticFiles
 import uvicorn
+import os
+from env import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN
+from twilio.rest import Client
+
+import threading
 
 from twilio.twiml.voice_response import VoiceResponse
 
 app = FastAPI()
 
+app.mount("/getAudio", StaticFiles(directory="recordings/output"), name="getAudio")
 
-# @app.get("/")
-# async def greeting():
-#     return "hello"
+# ------------------------------- Twilio client ------------------------------ #
+account_sid = TWILIO_ACCOUNT_SID
+auth_token = TWILIO_AUTH_TOKEN
+client = Client(account_sid, auth_token)
+
 
 @app.post("/")
 async def answer_call():
@@ -29,18 +38,43 @@ async def answer_call():
 async def answer_call(request: Request):
     req = await request.form()
     input_recording_url = req['RecordingUrl']
+    call_sid = req['CallSid']
+
     print(input_recording_url)
+    print(call_sid)
 
-    # text = answer_question(input_recording_url, 2)
+    t = threading.Thread(target=process_response,
+                         args=(call_sid, input_recording_url))
+    t.start()
+
+    print("Voice Response")
     resp = VoiceResponse()
-    if input_recording_url == None:
-        resp.say("Error please call again")
-    else:
-        resp.play(input_recording_url)
-
+    resp.pause(length=30)
     resp.hangup()
 
     return Response(content=str(resp), media_type="text/xml")
+
+
+def process_response(call_sid, input_recording_url):
+    try:
+        print(input_recording_url)
+        audio_timelist = []
+        text = answer_question_from_url(input_recording_url, "2",
+                                        "fileName", audio_timelist)
+        resp = VoiceResponse()
+        output_recording_url = "https://1a03-118-140-204-138.ngrok-free.app/getAudio/fileName.wav"
+
+        if output_recording_url == None:
+            resp.say("Error please call again")
+        else:
+            resp.play(output_recording_url)
+
+        resp.hangup()
+
+        call = client.calls(call_sid).update(twiml=str(resp))
+        call.to
+    except Exception as e:
+        print("Error", e)
 
 
 # -------------------------------- init server ------------------------------- #
--- text2speech_server/speech2test2speech.py
+++ text2speech_server/speech2test2speech.py
@@ -8,18 +8,23 @@ from google.oauth2 import service_account
 from os.path import splitext
 from pydub import AudioSegment
 import time
+from urllib.request import urlopen
+
 
 def wav_to_flac(wav_path):
     try:
         flac_path = "%s.flac" % splitext(wav_path)[0]
         song = AudioSegment.from_file(wav_path)
-        song.export(flac_path, format = "flac")
+        song.export(flac_path, format="flac")
     except Exception as e:
         print(e)
 
+
 def answer_question(input_audio, company_question, fileName, audio_timelist):
-    print("get question--- ", input_audio, "conmany--- ", company_question,"filename---  ", fileName,"audio--- ", audio_timelist)
-    x = requests.get(f"https://www.manhon95.online/users-questions?user-id={company_question}", headers={'Accept': 'application/json'})
+    print("get question--- ", input_audio, "conmany--- ", company_question,
+          "filename---  ", fileName, "audio--- ", audio_timelist)
+    x = requests.get(
+        f"https://www.manhon95.online/users-questions?user-id={company_question}", headers={'Accept': 'application/json'})
     print(x.json())
 
     try:
@@ -30,37 +35,91 @@ def answer_question(input_audio, company_question, fileName, audio_timelist):
         print("files_made")
 
         openai.api_key = f'{GPT_KEY}'
-        
+
         audio_file = open(f"{input_audio}", "rb")
         transcript = openai.Audio.transcribe("whisper-1", audio_file)
-        print("speech to text: ",transcript.text)
-        
+        print("speech to text: ", transcript.text)
+
         response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-                    {"role": "user","content": f"base on these Q and A {x.json()}. Only give the answer for the following question: {transcript.text}, if the question is empty reply with: Sorry, I cannot provide an answer with out a question"}
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": f"base on these Q and A {x.json()}. Only give the answer for the following question: {transcript.text}, if the question is empty reply with: Sorry, I cannot provide an answer with out a question"}
             ]
         )
 
-        print("chatGPT responds: ",response.choices[0].message.content)
+        print("chatGPT responds: ", response.choices[0].message.content)
 
         model_name = TTS.list_models()[7]
 
         tts = TTS(model_name)
 
-        tts = TTS(model_name="tts_models/multilingual/multi-dataset/your_tts", progress_bar=True, gpu=False)
-        tts.tts_to_file(f"{response.choices[0].message.content}", speaker_wav="recordings/speaker_voice/3.wav", language="en", file_path=f"recordings/output/{fileName}.wav")
-        audio_timelist.append({"path": f"recordings/{fileName}.wav", "timeStamp": time.time() })
+        tts = TTS(model_name="tts_models/multilingual/multi-dataset/your_tts",
+                  progress_bar=True, gpu=False)
+        tts.tts_to_file(f"{response.choices[0].message.content}", speaker_wav="recordings/speaker_voice/3.wav",
+                        language="en", file_path=f"recordings/output/{fileName}.wav")
+        audio_timelist.append(
+            {"path": f"recordings/{fileName}.wav", "timeStamp": time.time()})
         print(audio_timelist)
 
-        text = {"input": transcript.text, "output": response.choices[0].message.content}
+        text = {"input": transcript.text,
+                "output": response.choices[0].message.content}
         return text
-    
+
         # text = {"input": transcript.text, "output": response.choices[0].message.content}
         # return text
 
-
-    
     except Exception as e:
         print(e)
 
+
+def answer_question_from_url(input_audio_url, company_question, fileName, audio_timelist):
+    print("get question--- ", input_audio_url, "conmany--- ", company_question,
+          "filename---  ", fileName, "audio--- ", audio_timelist)
+    x = requests.get(
+        f"https://www.manhon95.online/users-questions?user-id={company_question}", headers={'Accept': 'application/json'})
+    print(x.json())
+
+    try:
+        if not os.path.exists('recordings'):
+            os.mkdir("recordings")
+            os.mkdir("recordings/output")
+            os.mkdir("recordings/speaker_voice")
+        print("files_made")
+
+        openai.api_key = f'{GPT_KEY}'
+        audio_file = open("recordings/input.wav", "wb")
+        audio_file.write(urlopen(input_audio_url).read())
+        audio_file = open("recordings/input.wav", "rb")
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
+        print("speech to text: ", transcript.text)
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": f"base on these Q and A {x.json()}. Only give the answer for the following question: {transcript.text}, if the question is empty reply with: Sorry, I cannot provide an answer with out a question"}
+            ]
+        )
+
+        print("chatGPT responds: ", response.choices[0].message.content)
+
+        model_name = TTS.list_models()[7]
+
+        tts = TTS(model_name)
+
+        tts = TTS(model_name="tts_models/multilingual/multi-dataset/your_tts",
+                  progress_bar=True, gpu=False)
+        tts.tts_to_file(f"{response.choices[0].message.content}", speaker_wav="recordings/speaker_voice/3.wav",
+                        language="en", file_path=f"recordings/output/{fileName}.wav")
+        audio_timelist.append(
+            {"path": f"recordings/{fileName}.wav", "timeStamp": time.time()})
+        print(audio_timelist)
+
+        text = {"input": transcript.text,
+                "output": response.choices[0].message.content}
+        return text
+
+        # text = {"input": transcript.text, "output": response.choices[0].message.content}
+        # return text
+
+    except Exception as e:
+        print("exception ", e)
