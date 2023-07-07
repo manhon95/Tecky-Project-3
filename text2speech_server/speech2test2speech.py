c9a055426cb79293a2508b2518d949ef8cfd6db9
Vincent Tse <king2003man2@gmail.com>
2023-07-07 17:48:50 +0800

    google auth

--- text2speech_server/speech2test2speech.py
+++ text2speech_server/speech2test2speech.py
@@ -1,3 +1,4 @@
+from TTS.api import TTS
 import requests
 import openai
 import os
@@ -40,14 +41,45 @@ def answer_question(input_audio, company_question, fileName, audio_timelist):
         transcript = openai.Audio.transcribe("whisper-1", audio_file)
         print("speech to text: ", transcript.text)
 
-        response = openai.ChatCompletion.create(
+        GPTresponse = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "user", "content": f"base on these Q and A {x.json()}. Only give the answer for the following question: {transcript.text}, if the question is empty reply with: Sorry, I cannot provide an answer with out a question"}
             ]
         )
 
-        print("chatGPT responds: ", response.choices[0].message.content)
+        print("chatGPT responds: ", GPTresponse.choices[0].message.content)
+
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="googleAPI_key_project3.json"
+
+        client = texttospeech.TextToSpeechClient()
+
+        synthesis_input = texttospeech.SynthesisInput(text=response.choices[0].message.content)
+
+        voice = texttospeech.VoiceSelectionParams(
+            language_code="en-US", name= "yue-HK-Standard-B",
+        )
+
+        # Select the type of audio file you want returned
+        audio_config = texttospeech.AudioConfig(
+            audio_encoding=texttospeech.AudioEncoding.MP3
+        )
+
+        # Perform the text-to-speech request on the text input with the selected
+        # voice parameters and audio file type
+        response = client.synthesize_speech(
+            input=synthesis_input, voice=voice, audio_config=audio_config
+        )
+
+        # The response's audio_content is binary.
+        with open(f'recordings/{fileName}.mp3', "wb") as out:
+        # Write the response to the output file.
+            out.write(response.audio_content)
+            print(f'Audio content written to file "{fileName}.mp3"')
+
+        text = {"input": transcript.text,
+                "output": GPTresponse.choices[0].message.content}
+        return text
 
         # model_name = TTS.list_models()[7]
 
@@ -57,13 +89,13 @@ def answer_question(input_audio, company_question, fileName, audio_timelist):
         #           progress_bar=True, gpu=False)
         # tts.tts_to_file(f"{response.choices[0].message.content}", speaker_wav="recordings/speaker_voice/3.wav",
         #                 language="en", file_path=f"recordings/output/{fileName}.wav")
-        audio_timelist.append(
-            {"path": f"recordings/{fileName}.wav", "timeStamp": time.time()})
-        print(audio_timelist)
+        # audio_timelist.append(
+        #     {"path": f"recordings/{fileName}.wav", "timeStamp": time.time()})
+        # print(audio_timelist)
 
-        text = {"input": transcript.text,
-                "output": response.choices[0].message.content}
-        return text
+        # text = {"input": transcript.text,
+        #         "output": response.choices[0].message.content}
+        # return text
 
         # text = {"input": transcript.text, "output": response.choices[0].message.content}
         # return text
@@ -93,18 +125,20 @@ def answer_question_from_url(input_audio_url, company_question, fileName, audio_
         transcript = openai.Audio.transcribe("whisper-1", audio_file)
         print("speech to text: ", transcript.text)
 
-        response = openai.ChatCompletion.create(
+        GPTresponse = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
                 {"role": "user", "content": f"base on these Q and A {x.json()}. Only give the answer for the following question: {transcript.text}, if the question is empty reply with: Sorry, I cannot provide an answer with out a question"}
             ]
         )
 
-        print("chatGPT responds: ", response.choices[0].message.content)
+        print("chatGPT responds: ", GPTresponse.choices[0].message.content)
+
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="googleAPI_key_project3.json"
 
         client = texttospeech.TextToSpeechClient()
 
-        synthesis_input = texttospeech.SynthesisInput(text=response.choices[0].message.content)
+        synthesis_input = texttospeech.SynthesisInput(text=GPTresponse.choices[0].message.content)
 
         voice = texttospeech.VoiceSelectionParams(
             language_code="en-US", name= "yue-HK-Standard-B",
@@ -126,6 +160,11 @@ def answer_question_from_url(input_audio_url, company_question, fileName, audio_
         # Write the response to the output file.
             out.write(response.audio_content)
             print(f'Audio content written to file "{fileName}.mp3"')
+
+        text = {"input": transcript.text,
+                "output": GPTresponse.choices[0].message.content}
+        return text
+
 
         # model_name = TTS.list_models()[7]
 
